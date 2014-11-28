31e976bde011aebe90e0661ee1cdb1a22b477819
wxx5433 <wxx5433@gmail.com>
2014-11-27 21:10:02 -0500

    finish Point class

--- src/Point2D.java
+++ src/Point2D.java
@@ -1,9 +1,40 @@
 
+/**
+ * 2D Point class
+ * @author Xiaoxiang Wu (xiaoxiaw)
+ * @author Ye Zhou (yezhou)
+ *
+ */
 public class Point2D {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// to which cluster the point belong
+	private int cluster;
+	private double x;
+	private double y;
+	
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
 	}
-
+	
+	public Point2D(double x, double y, int cluster) {
+		this(x, y);
+		this.cluster = cluster;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public void setCluster(int cluster) {
+		this.cluster = cluster;
+	}
+	
+	public int getCluster() {
+		return this.cluster;
+	}
 }
